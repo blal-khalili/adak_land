4ec4c898f97ce38646999181ce08ad0280384866
Sobhan K_G <Sobhan_Alipour@gmail.com>
2025-12-07 15:40:15 +0330

    add product model and change navbar and css

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=13)
-    avatar = models.ImageField(upload_to='user_avatar/',null=True)
+    avatar = models.ImageField(upload_to="user_avatar/", null=True)
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # 'api',
+    "api",
     "account",
 ]
 
@@ -128,3 +129,50 @@ AUTH_USER_MODEL = "account.User"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "media/"
 MEDIA_ROOT = "media/"
+
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Adack Land",
+    "copyright": "Sobhan Alipour",
+    "topmenu_links": [
+        {"app": "apps"},
+        {
+            "name": "Support",
+            "url": "https://github.com/farridav/django-jazzmin/issues",
+            "new_window": True,
+        },
+    ],
+    "show_ui_builder": False,
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": True,
+    "brand_colour": "navbar-primary",
+    "accent": "accent-warning",
+    "navbar": "navbar-warning navbar-light",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-warning",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "darkly",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success",
+    },
+}
