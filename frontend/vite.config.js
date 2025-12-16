d01d811861156fdb172ace86d052dbb0ae96d1db
blal-khalili <khalili.blal1@gmail.com>
2025-12-16 17:49:48 +0330

    mobile test server in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    port:5173,
+    // cors:true,
+    host:"192.168.25.23",
+    host:true,
+  },
 })
