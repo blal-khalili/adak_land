f01b47a4ce41ae8982df4148d02990cf70ffa631
Sahand-Alipour <Pikaso01so@gmail.com>
2026-06-18 15:00:18 +0330

    commit space

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserDetailAPIView
 
 urlpatterns = [
-    path('detail/<int:id>/', UserDetailAPIView.as_view()),
+    path("detail/<int:id>/", UserDetailAPIView.as_view()),
 ]
