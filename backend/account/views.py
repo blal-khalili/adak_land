40a20ecd975e3a3e5fb2e4059e96e4001d775add
blal-khalili <khalili.blal1@gmail.com>
2026-07-22 10:17:49 -0400

    implementing email send with django for registration logic

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,21 +1,27 @@
+import json
+import time
 from django.shortcuts import render
-from rest_framework.generics import RetrieveAPIView, CreateAPIView
+from django.http import HttpResponse
+from rest_framework.generics import RetrieveAPIView, CreateAPIView,RetrieveUpdateAPIView
 from .models import User
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
-from .serializers import OneUserSerializer,CreateUserSerializer
+from .serializers import OneUserSerializer,CreateUserSerializer,UserVrficationSerializer
 from django.core.mail import send_mail
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 
-
-# send_mail(
-#     'Subject here',
-#     'Here is the message.',
-#     'from@example.com',
-#     ['to@example.com'],
-#     fail_silently=False,
-# )
+def test_send_mail(request):
+    send_mail(
+        'موضوع پیام',
+        'کد تایید شما ۸۲۱۷۹ است.',
+        settings.EMAIL_HOST_USER,
+        ['example@gmail.com'],
+        fail_silently=False,
+    )
+    return HttpResponse('<h1>hi</h1>')
 
 class UserDetailAPIView(RetrieveAPIView):
     queryset = User.objects.all()
@@ -28,3 +34,15 @@ class UserCreateAPIView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = CreateUserSerializer
     permission_classes = [AllowAny]
+
+
+@csrf_exempt
+def user_vrfication(request,email):
+    data = json.loads(request.body)
+    user = User.objects.filter(email=data['email']).first()
+    if user.verification_code_timestamp + 120 < time.time():
+        print('code is expired')
+    else:
+        print('activate user')
+
+    return HttpResponse('<h1>hi</h1>')
---
+++ backend/test_api.py
@@ -0,0 +1,4 @@
+import requests
+
+
+# requests.post('http://127.0.0.1:8000/account/verify-account/example@gmail.com/',json={'email':'example@gmail.com','password':'212988'})
